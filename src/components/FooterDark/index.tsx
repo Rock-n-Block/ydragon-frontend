f02428f976fd66c79c50a2f48ff3c248159b924d
gavdniwe <dimanikus0106@gmail.com>
2021-06-10 17:52:35 +0300

    commented medium and discord links

--- src/components/FooterDark/index.tsx
+++ src/components/FooterDark/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../../assets/img/icons/logo.svg';
-import dis from '../../assets/img/socials/discord.svg';
-import md from '../../assets/img/socials/medium.svg';
+// import dis from '../../assets/img/socials/discord.svg';
+// import md from '../../assets/img/socials/medium.svg';
 import tg from '../../assets/img/socials/telegram.svg';
 import tw from '../../assets/img/socials/twitter.svg';
 
@@ -28,21 +28,31 @@ const Footer: React.FC = () => {
             </div>
 
             <div className="footer__socials">
-              <a href="/" className="footer__socials-item">
+              <a
+                href="https://t.me/ydrmain/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer__socials-item"
+              >
                 <img src={tg} alt="logo" width="16" height="16" />
               </a>
 
-              <a href="/" className="footer__socials-item">
+              <a
+                href="https://twitter.com/ydragons_"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer__socials-item"
+              >
                 <img src={tw} alt="logo" width="16" height="16" />
               </a>
 
-              <a href="/" className="footer__socials-item">
+              {/* <a href="/" className="footer__socials-item">
                 <img src={md} alt="logo" width="16" height="16" />
-              </a>
-
+              </a> */}
+              {/*
               <a href="/" className="footer__socials-item">
                 <img src={dis} alt="logo" width="16" height="16" />
-              </a>
+              </a> */}
             </div>
 
             <div className="footer__copyright">© 2021 YDRAGON</div>
