e6e88ec71e77f35fe8269e24f010d4cc2a9d11ff
TheJuze <dimanikus0106@gmail.com>
2021-06-15 13:35:30 +0300

    adding tables

--- src/components/Admin/Indexes/index.tsx
+++ src/components/Admin/Indexes/index.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import nextId from 'react-id-generator';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
@@ -76,44 +75,6 @@ const Indexes: React.FC = () => {
         <h2 className="section__title text-outline">Indexes</h2>
 
         <Button className="index-create-btn">create new index</Button>
-      </div>
-
-      <div className="indexs-table">
-        <div className="indexs-table__row indexs-table__row--head">
-          <div className="indexs-table__col">
-            <div className="indexs-table__sort indexs-table__sort--up">Name</div>
-          </div>
-          <div className="indexs-table__col">
-            <div className="indexs-table__sort">Market cap</div>
-          </div>
-          <div className="indexs-table__col">
-            <div className="indexs-table__sort">Price</div>
-          </div>
-          <div className="indexs-table__col">
-            <div className="indexs-table__sort">Cteated</div>
-          </div>
-        </div>
-
-        <div className="indexs-table__content">
-          {indexes?.map((index: IIndex) => (
-            <div className="indexs-table__row" key={nextId()}>
-              <Link to={`/admin/index/${index.id}`} className="indexs-table__col">
-                <div className="indexs-table__name">{index.name}</div>
-              </Link>
-              <div className="indexs-table__col">
-                <div className="indexs-table__market-cup">${index.market_cap}</div>
-              </div>
-              <div className="indexs-table__col">
-                <div className="indexs-table__price">${index.price}</div>
-              </div>
-              <div className="indexs-table__col">
-                <div className="indexs-table__created">
-                  {moment(new Date(index.created_at)).format('DD.MM.YYYY')}
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
 
       {indexes && <Table dataSource={dataSource} columns={columns} className="rebalance-table" />}
