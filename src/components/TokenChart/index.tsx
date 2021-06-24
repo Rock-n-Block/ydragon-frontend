465b57d3bbe8fdba2321f522760ec8146ee9f91b
KiryaFT <gusev.kirill@mywish.io>
2021-06-24 23:28:17 +0300

    Added buttons for toggle line chart data

--- src/components/TokenChart/index.tsx
+++ src/components/TokenChart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import axios from 'axios';
 import BigNumber from 'bignumber.js/bignumber';
@@ -10,9 +10,22 @@ interface TokenChartProps {
 }
 
 const TokenChart: React.FC<TokenChartProps> = ({ page }) => {
-  const refFetchData = useRef({});
-  const refData = useRef({});
-  const days = 1;
+  const [days, setDays] = useState('1');
+  const [refData, setRefData] = useState({
+    labels: ['1', '2', '3', '4', '5'],
+    datasets: [
+      {
+        data: [1, 2, 3, 4, 5],
+        fill: false,
+        backgroundColor: 'transparent',
+        borderColor: '#df3f3a',
+        borderWidth: 3,
+        pointBackgroundColor: '#df3f3a',
+        pointBorderColor: '#fff',
+        tension: 0.5,
+      },
+    ],
+  });
   const url: string =
     page === 'YDR Token'
       ? `https://api.coingecko.com/api/v3/coins/rubic/market_chart?vs_currency=usd&days=${days}`
@@ -25,8 +38,6 @@ const TokenChart: React.FC<TokenChartProps> = ({ page }) => {
   const diff = new BigNumber(value).toFixed(1).toString();
 
   const options = {
-    keepAspectRatio: false,
-    responsive: true,
     plugins: {
       legend: {
         display: false,
@@ -53,6 +64,31 @@ const TokenChart: React.FC<TokenChartProps> = ({ page }) => {
     },
   };
 
+  const toggleHandler = (event: any) => {
+    const btnsList = event.target.parentNode.children;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const item of btnsList) {
+      console.log(item.innerHTML);
+      item.className = item === event.target ? 'chart-panel-btn active' : 'chart-panel-btn';
+      switch (item.innerHTML) {
+        case '1d':
+          setDays('1');
+          break;
+        case '1m':
+          setDays('30');
+          break;
+        case '3m':
+          setDays('90');
+          break;
+        case 'ALL':
+          setDays('max');
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
   const getChartInfo = (data: any) => {
     const chartInfo: { labels: string[]; data: number[] } = {
       labels: [],
@@ -66,16 +102,18 @@ const TokenChart: React.FC<TokenChartProps> = ({ page }) => {
       });
     const lineChartData = {
       labels: chartInfo.labels,
-      datasets: {
-        data: chartInfo.data,
-        fill: false,
-        backgroundColor: 'transparent',
-        borderColor: '#df3f3a',
-        borderWidth: 3,
-        pointBackgroundColor: '#df3f3a',
-        pointBorderColor: '#fff',
-        tension: 0.5,
-      },
+      datasets: [
+        {
+          data: chartInfo.data,
+          fill: false,
+          backgroundColor: 'transparent',
+          borderColor: '#df3f3a',
+          borderWidth: 3,
+          pointBackgroundColor: '#df3f3a',
+          pointBorderColor: '#fff',
+          tension: 0.5,
+        },
+      ],
     };
     console.log('lineChartData:', lineChartData);
     return lineChartData;
@@ -84,16 +122,13 @@ const TokenChart: React.FC<TokenChartProps> = ({ page }) => {
   const axiosData = useCallback(() => {
     axios.get(url).then((res) => {
       console.log('Response:', res.data.market_caps);
-      refFetchData.current = res.data.market_caps;
-      console.log('refFetchData:', refFetchData.current);
-      refData.current = getChartInfo(refFetchData.current);
-      console.log('refData:', refData.current);
+      setRefData(getChartInfo(res.data.market_caps));
     });
-  }, [url]);
+  }, [setRefData, url]);
 
   useEffect(() => {
     axiosData();
-  }, [axiosData]);
+  }, [axiosData, days]);
 
   // useEffect(() => {
   //
@@ -111,26 +146,20 @@ const TokenChart: React.FC<TokenChartProps> = ({ page }) => {
             </div>
           </div>
         </div>
-        <div className="chart-panel-btns">
-          <button type="button" className="chart-panel-btn active">
-            1d
-          </button>
-          <button type="button" className="chart-panel-btn">
-            1m
-          </button>
-          <button type="button" className="chart-panel-btn">
-            3m
-          </button>
-          <button type="button" className="chart-panel-btn">
-            ALL
-          </button>
+        <div
+          role="button"
+          tabIndex={0}
+          className="chart-panel-btns"
+          onClick={toggleHandler}
+          onKeyDown={toggleHandler}
+        >
+          <div className="chart-panel-btn active">1d</div>
+          <div className="chart-panel-btn">1m</div>
+          <div className="chart-panel-btn">3m</div>
+          <div className="chart-panel-btn">ALL</div>
         </div>
       </div>
-      {Object.keys(refData.current).length ? (
-        <Line data={refData.current} options={options} type="line" height={400} />
-      ) : (
-        <></>
-      )}
+      {Object.keys(refData).length ? <Line data={refData} options={options} type="line" /> : <></>}
     </div>
   );
 };
