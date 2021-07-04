84a62a30815fc1c77063542b86b6d3871eb5fa20
overkam <superivan0755@gmail.com>
2021-07-04 23:49:50 +0300

    light theme for home page and header

--- src/App.tsx
+++ src/App.tsx
@@ -26,47 +26,48 @@ export const App: React.FC = observer(() => {
   const about = useRouteMatch('/about-us');
   const { theme } = useMst();
 
-
   const user = !!localStorage?.yd_address || false;
   const admin = !!localStorage?.yd_token || false;
 
   const addClass = () => {
     let result;
     if (main.isExact) {
-      result = `page-wrapper page-wrapper--home ${theme.value}`;
+      result = `page-wrapper page-wrapper--home`;
     } else if (about) {
-      result = `page-wrapper page-wrapper--about ${theme.value}`;
-    } else result = `page-wrapper ${theme.value}`;
+      result = `page-wrapper page-wrapper--about`;
+    } else result = `page-wrapper`;
     return result;
   };
 
   return (
-    <div className={addClass()}>
-      <Header />
+    <div className={theme.value}>
+      <div className={addClass()}>
+        <Header />
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        {/* <Route exact path="/auth">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          {/* <Route exact path="/auth">
           <Auth />
         </Route> */}
-        <GuardedRoute exact path="/index/:indexId" component={Index} auth={user} />
-        <GuardedRoute exact path="/ydrtoken" component={YdrToken} auth={user} />
-        <GuardedRoute exact path="/admin" component={Admin} auth={admin} />
-        <GuardedRoute exact path="/admin" component={Indexes} auth={admin} />
-        <GuardedRoute exact path="/admin/index/:indexId" component={AdminIndex} auth={admin} />
-        <GuardedRoute exact path="/staking" component={StakePage} auth={user} />
-        <GuardedRoute exact path="/indexes" component={IndexDashboard} auth={user} />
-        <Route exact path="/about-us">
-          <AboutUs />
-        </Route>
-        <Route component={NoPageFound} />
-      </Switch>
-      <MetamaskErrModal />
-      <InfoModal />
-      <GetInModal />
-      <Footer />
+          <GuardedRoute exact path="/index/:indexId" component={Index} auth={user} />
+          <GuardedRoute exact path="/ydrtoken" component={YdrToken} auth={user} />
+          <GuardedRoute exact path="/admin" component={Admin} auth={admin} />
+          <GuardedRoute exact path="/admin" component={Indexes} auth={admin} />
+          <GuardedRoute exact path="/admin/index/:indexId" component={AdminIndex} auth={admin} />
+          <GuardedRoute exact path="/staking" component={StakePage} auth={user} />
+          <GuardedRoute exact path="/indexes" component={IndexDashboard} auth={user} />
+          <Route exact path="/about-us">
+            <AboutUs />
+          </Route>
+          <Route component={NoPageFound} />
+        </Switch>
+        <MetamaskErrModal />
+        <InfoModal />
+        <GetInModal />
+        <Footer />
+      </div>
     </div>
   );
 });
