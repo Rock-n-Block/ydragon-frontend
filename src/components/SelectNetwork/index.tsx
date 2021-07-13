3ee4e599e22a1c07f488447e4845384faf32d175
gavdniwe <dimanikus0106@gmail.com>
2021-07-13 13:41:36 +0300

    get networks and store them to mobx

--- src/components/SelectNetwork/index.tsx
+++ src/components/SelectNetwork/index.tsx
@@ -4,14 +4,15 @@ import { observer } from 'mobx-react-lite';
 
 import { useWalletConnectorContext } from '../../services/walletConnect';
 import { useMst } from '../../store/store';
-import arrow from'../../assets/img/icons/icon-arrow-yellow.svg'
-import bncLight from'../../assets/img/icons/icon-binance-light.svg'
-import bncDark from'../../assets/img/icons/icon-binance-dark.svg'
-import plgLight from'../../assets/img/icons/icon-polygon-light.svg'
-import plgDark from'../../assets/img/icons/icon-polygon-dark.svg'
+import arrow from '../../assets/img/icons/icon-arrow-yellow.svg';
+import bncLight from '../../assets/img/icons/icon-binance-light.svg';
+import bncDark from '../../assets/img/icons/icon-binance-dark.svg';
+import plgLight from '../../assets/img/icons/icon-polygon-light.svg';
+import plgDark from '../../assets/img/icons/icon-polygon-dark.svg';
 
 import './SelectNetwork.scss';
 import TokenMini from '../TokenMini';
+import { networksApi } from '../../services/api';
 
 const { Option } = Select;
 
@@ -64,6 +65,18 @@ const SelectNetwork: React.FC = observer(() => {
   const walletConnector = useWalletConnectorContext();
   const [pickedChain, setPickedChain] = useState<ChainTypes>();
 
+  const getNetworks = useCallback(() => {
+    networksApi
+      .getNetworks()
+      .then(({ data }) => {
+        console.log(data);
+        networks.setNetworks(data);
+      })
+      .catch((err) => {
+        const { response } = err;
+        console.log(response);
+      });
+  }, [networks]);
   const getCurrentChain = useCallback(() => {
     walletConnector.metamaskService.ethGetCurrentChain().then((currentChainId: string) => {
       Object.keys(chains).forEach((key) => {
@@ -97,6 +110,10 @@ const SelectNetwork: React.FC = observer(() => {
   };
 
   useEffect(() => {
+    getNetworks();
+  }, [getNetworks]);
+
+  useEffect(() => {
     getCurrentChain();
   }, [getCurrentChain]);
 
@@ -114,12 +131,28 @@ const SelectNetwork: React.FC = observer(() => {
       placeholder="Select network"
       onSelect={switchChain}
       style={{ width: 120 }}
-      className='select-network'
-      suffixIcon={<img className='select__arrow' alt='' src={arrow} />}
-      dropdownClassName='select-network__dropdown'
+      className="select-network"
+      suffixIcon={<img className="select__arrow" alt="" src={arrow} />}
+      dropdownClassName="select-network__dropdown"
     >
-      <Option value="bnbt"><TokenMini name='' icon={theme.value === 'dark' ? bncDark : bncLight} width="26" height="26" />BSC</Option>
-      <Option value="tmatic"><TokenMini name='' icon={theme.value === 'dark' ? plgDark : plgLight} width="26" height="26" />Polygon</Option>
+      <Option value="bnbt">
+        <TokenMini
+          name=""
+          icon={theme.value === 'dark' ? bncDark : bncLight}
+          width="26"
+          height="26"
+        />
+        BSC
+      </Option>
+      <Option value="tmatic">
+        <TokenMini
+          name=""
+          icon={theme.value === 'dark' ? plgDark : plgLight}
+          width="26"
+          height="26"
+        />
+        Polygon
+      </Option>
     </Select>
   );
 });
--- src/store/Networks.ts
+++ src/store/Networks.ts
@@ -1,14 +1,33 @@
 import { applySnapshot, types } from 'mobx-state-tree';
 
-export const Networks = types.model({ id: types.string }).actions((self) => {
-  const setId = (id: string) => {
-    self.id = id;
-  };
-  const update = (networkData: any) => {
-    applySnapshot(self, networkData);
-  };
-  return {
-    setId,
-    update,
-  };
+const Network = types.model({
+  id: types.number,
+  name: types.string,
+  fabric_address: types.string,
+  oracle_address: types.string,
+  staking_address: types.string,
+  endpoint: types.string,
+  needs_middleware: types.boolean,
 });
+
+export const Networks = types
+  .model({
+    id: types.string,
+    networksList: types.optional(types.array(Network), []),
+  })
+  .actions((self) => {
+    const setId = (id: string) => {
+      self.id = id;
+    };
+    const setNetworks = (networks: any) => {
+      self.networksList = networks;
+    };
+    const update = (networkData: any) => {
+      applySnapshot(self, networkData);
+    };
+    return {
+      setId,
+      setNetworks,
+      update,
+    };
+  });
