cdbe344c1adc1f9f03c9b060cce0ee659fc65333
ottebrut <ott.andrey.ao@gmail.com>
2021-08-14 15:09:41 +0300

    Bridge layout is updated

--- src/pages/Bridge/index.tsx
+++ src/pages/Bridge/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 
-import arrowDown from '../../assets/img/icons/icon-arrow-down-orange.svg';
-import indexLogo from '../../assets/img/icons/logo-index.svg';
+import bscLogo from '../../assets/img/icons/blockchains/bsc.svg';
+import ethLogo from '../../assets/img/icons/blockchains/eth.svg';
 import link from '../../assets/img/icons/icon-link.svg';
+import indexLogo from '../../assets/img/icons/logo-index.svg';
+import Dropdown from '../../components/Bridge/Dropdown';
 
 import './Bridge.scss';
 
@@ -25,105 +27,117 @@ const indexes = [
   },
 ];
 
+const blockchains = [
+  {
+    img: ethLogo,
+    title: 'Ethereum',
+  },
+  {
+    img: bscLogo,
+    title: 'Binance Smart Chain',
+  },
+];
+
 const Bridge: React.FC = () => {
-  const [isSelectOpen, setIsSelectOpen] = useState(false);
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [fromBlockchainIndex, setFromBlockchainIndex] = useState(0);
+  const [toBlockchainIndex, setToBlockchainIndex] = useState(1);
+  const [activeTokenIndex, setActiveTokenIndex] = useState(0);
+
+  const setBlockchainIndex = (type: 'from' | 'to', index: number): void => {
+    if (
+      (type === 'from' && index === fromBlockchainIndex) ||
+      (type === 'to' && index === toBlockchainIndex)
+    ) {
+      return;
+    }
+
+    const oppositeIndex = index === 0 ? 1 : 0;
+    if (type === 'from') {
+      setFromBlockchainIndex(index);
+      setToBlockchainIndex(oppositeIndex);
+    } else {
+      setToBlockchainIndex(index);
+      setFromBlockchainIndex(oppositeIndex);
+    }
+  };
 
   return (
     <div className="bridge">
-      <form className="form" action="submit">
-        <div className="form__header">
-          <div className="form__header__title">From</div>
-        </div>
-        <div className="box form__network">
-          <div className="box__items__item">
-            <div className="box__items__item__title">Ethereum</div>
+      <form className="form">
+        <div className="form__item">
+          <div className="form__item__header">
+            <div className="form__item__header__title">Choose Token</div>
           </div>
+          <Dropdown
+            items={indexes}
+            activeIndex={activeTokenIndex}
+            setActiveIndex={setActiveTokenIndex}
+          />
         </div>
-        <div className="form__header">
-          <div className="form__header__title">To</div>
+
+        <div className="form__item">
+          <div className="form__item__header">
+            <div className="form__item__header__title">From</div>
+          </div>
+          <Dropdown
+            items={blockchains}
+            activeIndex={fromBlockchainIndex}
+            setActiveIndex={(index) => setBlockchainIndex('from', index)}
+          />
         </div>
-        <div className={`gradient-box ${isSelectOpen ? 'open' : null}`}>
-          <div className={`box form__select ${isSelectOpen ? 'open' : null}`}>
-            <div className="box__items">
-              <div className="box__items__item">
-                <div className="box__items__item__logo">
-                  <img src={indexes[activeIndex].img} alt="index logo" />
-                </div>
-                <div className="box__items__item__title">{indexes[activeIndex].title}</div>
+
+        <div className="form__item">
+          <div className="form__item__header">
+            <div className="form__item__header__title">To</div>
+          </div>
+          <Dropdown
+            items={blockchains}
+            activeIndex={toBlockchainIndex}
+            setActiveIndex={(index) => setBlockchainIndex('to', index)}
+          />
+        </div>
+
+        <div className="form__item">
+          <div className="form__item__header">
+            <div className="form__item__header__title">Amount</div>
+            <div className="form__item__header__link">
+              <div className="form__item__header__link__text">ETHERSCAN KYP</div>
+              <img src={link} alt="etherscan link" />
+            </div>
+          </div>
+          <div className="gradient-box">
+            <div className="box form__item__input">
+              <input type="text" placeholder="Enter amount" />
+              <div className="form__item__input__send-max">
+                <div className="form__item__input__send-max__text">SEND MAX</div>
               </div>
-              {isSelectOpen
-                ? indexes.map((index, id) => {
-                    if (id !== activeIndex)
-                      return (
-                        <div
-                          className="box__items__item"
-                          role="button"
-                          tabIndex={0}
-                          onKeyPress={() => console.log('change active index')}
-                          key={index.title}
-                          onClick={() => {
-                            setActiveIndex(id);
-                            setIsSelectOpen(false);
-                          }}
-                        >
-                          <div className="box__items__item__logo">
-                            <img src={index.img} alt="index logo" />
-                          </div>
-                          <div className="box__items__item__title grey">{index.title}</div>
-                        </div>
-                      );
-                    return null;
-                  })
-                : null}
             </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className={`form__select__switch ${isSelectOpen ? 'open' : 'close'}`}
-              onClick={() => setIsSelectOpen(!isSelectOpen)}
-              onKeyPress={() => console.log('keypress')}
-            >
-              <img src={arrowDown} alt="arrow down orange" />
+          </div>
+          <div className="form__item__footer">
+            <div className="form__item__footer__fee">
+              <div className="form__item__footer__fee__text">Fee: 1</div>
+            </div>
+            <div className="form__item__footer__min-amount">Minimum amount: 0.00546 KYP</div>
+          </div>
+        </div>
+
+        <div className="form__item">
+          <div className="form__item__header">
+            <div className="form__item__header__title">You will receive</div>
+            <div className="form__item__header__link">
+              <div className="form__item__header__link__text">ETHERSCAN KYP</div>
+              <img src={link} alt="etherscan link" />
+            </div>
+          </div>
+          <div className="gradient-box">
+            <div className="box">
+              <div className="box__value">544.4115</div>
             </div>
           </div>
         </div>
-        <div className="form__header">
-          <div className="form__header__title">Amount</div>
-          <div className="form__header__link">
-            <div className="form__header__link__text">ETHERSCAN KYP</div>
-            <img src={link} alt="etherscan link" />
-          </div>
-        </div>
-        <div className="gradient-box">
-          <div className="box form__input">
-            <input type="text" placeholder="Enter amount" />
-            <div className="form__input__send-max">
-              <div className="form__input__send-max__text">SEND MAX</div>
-            </div>
-          </div>
-        </div>
-        <div className="form__footer">
-          <div className="form__footer__fee">
-            <div className="form__footer__fee__text">Fee: 1</div>
-          </div>
-          <div className="form__footer__min-amount">Minimum amount: 0.00546 KYP</div>
-        </div>
-        <div className="form__header">
-          <div className="form__header__title">You will receive</div>
-          <div className="form__header__link">
-            <div className="form__header__link__text">ETHERSCAN KYP</div>
-            <img src={link} alt="etherscan link" />
-          </div>
-        </div>
-        <div className="gradient-box">
-          <div className="box">
-            <div className="box__value">544.4115</div>
-          </div>
-        </div>
-        <div className="form__note">
-          Note: Transfer from Ethereum Network with Metamask wallet
-        </div>
+
+        <div className="form__note">Note: Transfer from Ethereum Network with Metamask wallet</div>
+
         <div className="form__submit">
           <div className="form__submit__button">
             <div className="form__submit__button__text">SWAP</div>
