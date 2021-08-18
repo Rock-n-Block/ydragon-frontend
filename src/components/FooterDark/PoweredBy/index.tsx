52531421bcb4d3ba96962cfe88ed992298ec6ecc
gavdniwe <dimanikus0106@gmail.com>
2021-08-18 15:33:20 +0300

    added light powered-by styles

--- src/components/FooterDark/PoweredBy/index.tsx
+++ src/components/FooterDark/PoweredBy/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './PoweredBy.scss';
-import rnbLogo from '../../../assets/img/icons/rnb-logo.svg';
+import rnbLogoD from '../../../assets/img/icons/rnb-logo.svg';
+import rnbLogoL from '../../../assets/img/icons/rnb-logo-light.svg';
+import { observer } from 'mobx-react-lite';
+import { useMst } from '../../../store/store';
 
-const PoweredBy: React.FC = () => {
+const PoweredBy: React.FC = observer(() => {
+  const { theme } = useMst();
+
   return (
-    <div className="powered">
-      <a href="https://rocknblock.io/" target="_blank" rel="noopener noreferrer">
-        <p className="powered-by">Powered by</p>
-        <img src={rnbLogo} className="powered-logo" alt="Rock'n'Block logo" />
-      </a>
-    </div>
+    <a className="powered" href="https://rocknblock.io/" target="_blank" rel="noopener noreferrer">
+      <p className="powered-by">Powered by</p>
+      <img
+        src={theme.value === 'dark' ? rnbLogoD : rnbLogoL}
+        className="powered-logo"
+        alt="Rock'n'Block logo"
+      />
+    </a>
   );
-};
+});
 
 export default PoweredBy;
