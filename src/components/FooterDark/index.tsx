6e24e59de73dd16c2d8e2db57a7066cde1c248b2
gavdniwe <dimanikus0106@gmail.com>
2021-08-23 14:44:01 +0300

    updated socials

--- src/components/FooterDark/index.tsx
+++ src/components/FooterDark/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { NavHashLink } from 'react-router-hash-link';
+// import PoweredBy from './PoweredBy';
 
 import logo from '../../assets/img/icons/logo.svg';
 // import dis from '../../assets/img/socials/discord.svg';
 import md from '../../assets/img/socials/medium.svg';
 import tg from '../../assets/img/socials/telegram.svg';
 import tw from '../../assets/img/socials/twitter.svg';
+import coingecko from '../../assets/img/socials/coingecko.svg';
+import coinmarketcapD from '../../assets/img/socials/coinmarketcap.svg';
+import coinmarketcapL from '../../assets/img/socials/coinmarketcap-light.svg';
+import nomics from '../../assets/img/socials/nomics.png';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import Whitepaper from '../../assets/pdf/YD WP.pdf';
 
 import './Footer.scss';
+import { DARK, useMst } from '../../store/store';
+import { observer } from 'mobx-react-lite';
 
-const Footer: React.FC = () => {
+const Footer: React.FC = observer(() => {
+  const { theme } = useMst();
   return (
     <footer className="footer">
       <div className="container">
@@ -32,42 +40,75 @@ const Footer: React.FC = () => {
             </div>
 
             <div className="footer__socials">
-              <a
-                href="https://t.me/ydrmain/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="footer__socials-item footer__socials-tg"
-              >
-                <img src={tg} alt="logo" width="16" height="16" />
-                <span>Channel</span>
-              </a>
-              <a
-                href="https://t.me/ydragonchat"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="footer__socials-item footer__socials-tg"
-              >
-                <img src={tg} alt="logo" width="16" height="16" />
-                <span>Chat</span>
-              </a>
+              <div className="footer__socials-group">
+                <a
+                  href="https://t.me/ydrmain/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="footer__socials-item footer__socials-tg"
+                >
+                  <img src={tg} alt="telegram channel" width="16" height="16" />
+                  <span>Channel</span>
+                </a>
 
-              <a
-                href="https://twitter.com/ydragons_"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="footer__socials-item"
-              >
-                <img src={tw} alt="logo" width="16" height="16" />
-              </a>
-
-              <a
-                href="https://medium.com/ydragon-io"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="footer__socials-item"
-              >
-                <img src={md} alt="logo" width="16" height="16" />
-              </a>
+                <a
+                  href="https://t.me/ydragonchat"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="footer__socials-item footer__socials-tg"
+                >
+                  <img src={tg} alt="telegram chat" width="16" height="16" />
+                  <span>Chat</span>
+                </a>
+                <a
+                  href="https://twitter.com/ydragons_"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="footer__socials-item"
+                >
+                  <img src={tw} alt="twitter" width="16" height="16" />
+                </a>
+                <a
+                  href="https://medium.com/ydragon-io"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="footer__socials-item"
+                >
+                  <img src={md} alt="medium" width="16" height="16" />
+                </a>
+              </div>
+              <div className="footer__socials-divider" />
+              <div className="footer__socials-group">
+                <a
+                  href="https://coinmarketcap.com/currencies/ydragon/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="footer__socials-item"
+                >
+                  <img
+                    src={theme.value === DARK ? coinmarketcapD : coinmarketcapL}
+                    alt="coinmarketcap"
+                    width="16"
+                    height="16"
+                  />
+                </a>
+                <a
+                  href="https://www.coingecko.com/en/coins/ydragon"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="footer__socials-item"
+                >
+                  <img src={coingecko} alt="coingecko" width="16" height="16" />
+                </a>
+                <a
+                  href="https://nomics.com/assets/ydr-ydragon"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="footer__socials-item"
+                >
+                  <img src={nomics} alt="nomics" width="16" height="16" />
+                </a>
+              </div>
               {/*
               <a href="/" className="footer__socials-item">
                 <img src={dis} alt="logo" width="16" height="16" />
@@ -112,10 +153,8 @@ const Footer: React.FC = () => {
             </div>
           </div>
           {/*
-
           <div className="footer__col">
             <div className="footer__links-title">DeFi</div>
-
             <div className="footer__links">
               <span className="isDisabled">
                 <a href="/">Index Products</a>
@@ -126,9 +165,10 @@ const Footer: React.FC = () => {
             </div>
           </div> */}
         </div>
+        {/* <PoweredBy /> */}
       </div>
     </footer>
   );
-};
+});
 
 export default Footer;
