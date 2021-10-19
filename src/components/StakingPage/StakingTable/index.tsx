78e97dbbb7ecf3a139deb8738e4954160f760a66
dnotrad <lentsd@bk.ru>
2021-10-19 18:14:43 +0300

    add table markup, adaptive and light theme

--- src/components/StakingPage/StakingTable/index.tsx
+++ src/components/StakingPage/StakingTable/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import StakingTableRow from './StakingTableRow';
+
+import './StakingTable.scss';
+
 const StakingTable: React.FC = () => {
-  return <section className="section">table</section>;
+  return (
+    <section className="section">
+      <div className="staking-table">
+        <div className="staking-table_header">
+          <div className="staking-table_header__cell" />
+          <div className="staking-table_header__cell">Balance</div>
+          <div className="staking-table_header__cell">Deposited</div>
+          <div className="staking-table_header__cell">Your Awards</div>
+          <div className="staking-table_header__cell">TVL</div>
+          <div className="staking-table_header__cell" />
+          <div className="staking-table_header__cell" />
+        </div>
+        <div className="staking-table_body">
+          <StakingTableRow />
+          <StakingTableRow />
+          <StakingTableRow />
+          <StakingTableRow />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default StakingTable;
