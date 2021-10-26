f781fca805adc8bce96bed11293eef9ad683d147
TheJuze <dimanikus0106@gmail.com>
2021-10-26 14:15:57 +0300

    removed basic_tokens.ts

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,5 +1,4 @@
 export { default as accountsApi } from './accounts';
-export { default as basicTokensApi } from './basic_tokens';
 export { default as indexesApi } from './indexes';
 export { default as ratesApi } from './rates';
 export { default as coinsApi } from './coins';
