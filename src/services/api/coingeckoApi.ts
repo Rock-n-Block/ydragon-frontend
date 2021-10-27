11f92eeb818681ac726d5fab46772ec06b4f9e70
TheJuze <dimanikus0106@gmail.com>
2021-10-27 19:37:09 +0300

    added getYdrCurrentPrice request

--- src/services/api/coingeckoApi.ts
+++ src/services/api/coingeckoApi.ts
@@ -2,10 +2,17 @@ import axios from '../../core/axios';
 
 export default {
   getYDRTokensChart: (days: string) =>
-    axios.get(`https://api.coingecko.com/api/v3/coins/ydragon/market_chart`, {
+    axios.get('https://api.coingecko.com/api/v3/coins/ydragon/market_chart', {
       params: {
         vs_currency: 'usd',
         days,
+      },
+    }),
+  getYdrCurrentPrice: () =>
+    axios.get('https://api.coingecko.com/api/v3/simple/price', {
+      params: {
+        ids: 'ydragon',
+        vs_currency: 'usd',
       },
     }),
 };
