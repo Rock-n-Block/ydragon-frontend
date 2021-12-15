3c2317f4f71851716eae89b6d370a18cb339907c
Daniil <rusm9snik2013@yandex.ru>
2021-12-15 18:32:02 +0300

    header : add changeable theme via scroll

---
+++ src/hooks/useIsHeaderBackground.ts
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const UseIsHeaderBackground = (scrollYAmount: number) => {
+  const [isHeaderBackground, setIsHeaderBackground] = useState(false);
+
+  const [scrollY, setScrollY] = useState(0);
+
+  const logIt = useCallback(() => {
+    setScrollY(window.pageYOffset);
+    if (scrollY > scrollYAmount) {
+      setIsHeaderBackground(true);
+    } else setIsHeaderBackground(false);
+  }, [scrollY, scrollYAmount]);
+
+  useEffect(() => {
+    const watchScroll = () => {
+      window.addEventListener('scroll', logIt);
+    };
+    watchScroll();
+    return () => {
+      window.removeEventListener('scroll', logIt);
+    };
+  });
+  return isHeaderBackground;
+};
+
+export default UseIsHeaderBackground;
