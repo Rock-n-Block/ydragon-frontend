aeed750bb97ea02f271959d81a04909dd5dd30f6
TheJuze <dimanikus0106@gmail.com>
2022-02-04 16:53:39 +0300

    feat(Partners): changed partners block
    Added more partners, changed partners displaying from slider to tabs, added auto-tab switching in 5s
    
    Closes: -

--- src/components/HomeDark/Partners/index.tsx
+++ src/components/HomeDark/Partners/index.tsx
@@ -1,170 +1,40 @@
-import React, { useCallback } from 'react';
-import nextId from 'react-id-generator';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { observer } from 'mobx-react';
 import SwiperCore, { Autoplay } from 'swiper/core';
-import { Swiper, SwiperSlide } from 'swiper/react';
 
-import bscPad from '../../../assets/img/icons/partners/bsc-pad.svg';
-import hyperconnectD from '../../../assets/img/icons/partners/hyperconnect-dark.svg';
-import hyperconnectL from '../../../assets/img/icons/partners/hyperconnect-light.svg';
-import lossLessD from '../../../assets/img/icons/partners/lossless-dark.svg';
-import lossLessL from '../../../assets/img/icons/partners/lossless-light.svg';
-import magnusD from '../../../assets/img/icons/partners/magnus-dark.png';
-import magnusL from '../../../assets/img/icons/partners/magnus-light.png';
-import marshlandD from '../../../assets/img/icons/partners/marshland-dark.png';
-import marshlandL from '../../../assets/img/icons/partners/marshland-light.png';
-import moonwolfD from '../../../assets/img/icons/partners/moonwolf-dark.png';
-import moonwolfL from '../../../assets/img/icons/partners/moonwolf-light.png';
-import bscStationL from '../../../assets/img/icons/partners/bsc-station-light.png';
-import bscStationD from '../../../assets/img/icons/partners/bsc-station-dark.png';
-import polygonD from '../../../assets/img/icons/partners/polygon-dark.svg';
-import polygonL from '../../../assets/img/icons/partners/polygon-light.svg';
-import qiDaoD from '../../../assets/img/icons/partners/qi-dao-dark.png';
-import mateLD from '../../../assets/img/icons/partners/mate-dark.png';
-import drakuruLD from '../../../assets/img/icons/partners/drakuru-dark.png';
-import qiDaoL from '../../../assets/img/icons/partners/qi-dao-light.png';
-import trustSwapD from '../../../assets/img/icons/partners/trust-swap-dark.svg';
-import unimexD from '../../../assets/img/icons/partners/unimex-light.svg';
-import unimexL from '../../../assets/img/icons/partners/unimex-dark.svg';
-import trustSwapL from '../../../assets/img/icons/partners/trust-swap-light.svg';
-import cyberfiL from '../../../assets/img/icons/partners/cyberfi-dark.svg';
-import cyberfiD from '../../../assets/img/icons/partners/cyberfi-light.svg';
-import apeswapLD from '../../../assets/img/icons/partners/apeswap-dark.svg';
-import certikD from '../../../assets/img/icons/partners/certik-dark.png';
-import certikL from '../../../assets/img/icons/partners/certik-light.png';
 import { useMst } from '../../../store/store';
-
+import { partners, partnersSetArray, TPartnerSet } from './partners';
 import 'swiper/swiper.scss';
-import './Protocols.scss';
+import './Partners.scss';
 
 SwiperCore.use([Autoplay]);
 
 const Partners: React.FC = observer(() => {
   const { theme } = useMst();
-  const partners = useCallback(() => {
-    const isLight = theme.value !== 'dark';
-    return [
-      <a href="https://moonwolf.io/" target="_blank" className="protocols__link" rel="noreferrer">
-        <img src={isLight ? moonwolfL : moonwolfD} alt="moonwolf" width="275" height="72" />
-      </a>,
-      <a
-        href="https://unimex.network/"
-        target="_blank"
-        className="protocols__link"
-        rel="noreferrer"
-      >
-        <img src={isLight ? unimexL : unimexD} alt="unimex logo" width="239" height="64" />
-      </a>,
-      <a
-        href="https://www.certik.org/"
-        target="_blank"
-        className="protocols__link"
-        rel="noreferrer"
-      >
-        <img src={isLight ? certikL : certikD} alt="certik logo" width="311" height="73" />
-      </a>,
-      <a
-        href="https://apeswap.finance/"
-        target="_blank"
-        className="protocols__link"
-        rel="noreferrer"
-      >
-        <img src={isLight ? apeswapLD : apeswapLD} alt="apeswap logo" width="311" height="84" />
-      </a>,
-      <a href="https://orakuru.io/" target="_blank" className="protocols__link" rel="noreferrer">
-        <img src={isLight ? drakuruLD : drakuruLD} alt="orakuru logo" width="118" height="118" />
-      </a>,
-      <a
-        href="https://samurai.cyberfi.tech/"
-        target="_blank"
-        className="protocols__link"
-        rel="noreferrer"
-      >
-        <img src={isLight ? cyberfiL : cyberfiD} alt="cyberfi logo" width="283" height="77" />
-      </a>,
-      <a href="https://usemate.com/" target="_blank" className="protocols__link" rel="noreferrer">
-        <img src={isLight ? mateLD : mateLD} alt="mate logo" width="183" height="51" />
-      </a>,
-      <a
-        href="https://bscstation.finance/"
-        target="_blank"
-        className="protocols__link"
-        rel="noreferrer"
-      >
-        <img
-          src={isLight ? bscStationL : bscStationD}
-          alt="bscStation logo"
-          width="186"
-          height="72"
-        />
-      </a>,
-      <a
-        href="https://www.marshlandcapital.com/"
-        target="_blank"
-        className="protocols__link"
-        rel="noreferrer"
-      >
-        <img src={isLight ? marshlandL : marshlandD} alt="marshland" width="92" height="78" />
-      </a>,
-      <a href="https://lossless.cash/" target="_blank" className="protocols__link" rel="noreferrer">
-        <img src={isLight ? lossLessL : lossLessD} alt="lossLess" width="291" height="64" />
-      </a>,
-      <a
-        href="https://magnusdigitalassets.com/"
-        target="_blank"
-        className="protocols__link"
-        rel="noreferrer"
-      >
-        <img src={isLight ? magnusL : magnusD} alt="magnus capital" width="154" height="78" />
-      </a>,
-      <a
-        href="https://polygon.technology/"
-        target="_blank"
-        className="protocols__link"
-        rel="noreferrer"
-      >
-        <img src={isLight ? polygonL : polygonD} alt="polygon" width="280" height="62" />
-      </a>,
-      <a href="https://bscpad.com/" target="_blank" className="protocols__link" rel="noreferrer">
-        <img src={bscPad} alt="bscPad" width="152" height="64" />
-      </a>,
-      <a
-        href="https://www.iconhyperconnect.com/"
-        target="_blank"
-        className="protocols__link"
-        rel="noreferrer"
-      >
-        <img
-          src={isLight ? hyperconnectL : hyperconnectD}
-          alt="icon hyperconnect p-rep"
-          width="290"
-          height="64"
-        />
-      </a>,
-      <a
-        href="https://www.mai.finance/"
-        target="_blank"
-        className="protocols__link"
-        rel="noreferrer"
-      >
-        <img src={isLight ? qiDaoL : qiDaoD} alt="Qi Dao" width="160" height="72" />
-      </a>,
-      <a
-        href="https://www.trustswap.org/"
-        target="_blank"
-        className="protocols__link"
-        rel="noreferrer"
-      >
-        <img src={isLight ? trustSwapL : trustSwapD} alt="trustSwap" width="312" height="52" />
-      </a>,
-    ];
+  const [partnersTab, setPartnersTab] = useState<TPartnerSet>('Binance Smart Chain');
+
+  const isLight = useMemo(() => {
+    return theme.value !== 'dark';
   }, [theme.value]);
+
+  const toNextTab = useCallback(() => {
+    console.log('toNextTab called');
+    const indexOfCurrentTab = partnersSetArray.indexOf(partnersTab);
+    const isTabLast = indexOfCurrentTab === partnersSetArray.length - 1;
+    const nextTab = isTabLast ? partnersSetArray[0] : partnersSetArray[indexOfCurrentTab + 1];
+    setPartnersTab(nextTab);
+  }, [partnersTab]);
+
+  useEffect(() => {
+    const switchTabInterval = setInterval(toNextTab, 5000);
+    return () => clearInterval(switchTabInterval);
+  }, [toNextTab]);
 
   return (
     <section className="section">
-      <h3 className="section__title text-outline">Our partners</h3>
-      <div className="protocols">
-        <div className="protocols__row">
+      <h3 className="section__title text-outline">protocols & partners</h3>
+      <div className="partners">
+        {/* <div className="partners__row">
           <Swiper
             slidesPerView={1}
             freeMode
@@ -195,42 +65,36 @@ const Partners: React.FC = observer(() => {
               </SwiperSlide>
             ))}
           </Swiper>
+        </div> */}
+        <div className="partners__tabs">
+          {partnersSetArray.map((partnersTabName) => (
+            <button
+              type="button"
+              key={`partners-tab_${partnersTabName}`}
+              className={`partners__tabs_btn ${
+                partnersTabName === partnersTab ? 'partners__tabs_btn-active' : ''
+              }`}
+              onClick={() => {
+                setPartnersTab(partnersTabName);
+              }}
+            >
+              {partnersTabName}
+            </button>
+          ))}
         </div>
-        <div className="protocols__row">
-          <Swiper
-            slidesPerView={1}
-            freeMode
-            loop
-            allowTouchMove={false}
-            autoplay={{
-              delay: 0,
-              reverseDirection: true,
-              disableOnInteraction: false,
-            }}
-            speed={10000}
-            breakpoints={{
-              1200: {
-                slidesPerView: 3,
-                spaceBetween: 24,
-              },
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 24,
-              },
-              576: {
-                slidesPerView: 1,
-                // spaceBetween: 24,
-              },
-            }}
-          >
-            {partners()
-              .reverse()
-              .map((partner) => (
-                <SwiperSlide key={nextId()} className="protocols__item">
-                  {partner}
-                </SwiperSlide>
-              ))}
-          </Swiper>
+        <div className="partners__block">
+          {partners[partnersTab].map((partner) => (
+            <div className="partners__item" key={`partner ${partner.name}`}>
+              <a href={partner.link} target="_blank" className="partners__link" rel="noreferrer">
+                <img
+                  src={isLight ? partner.image.light : partner.image.dark}
+                  alt={partner.name}
+                  width={partner.width}
+                  height={partner.height}
+                />
+              </a>
+            </div>
+          ))}
         </div>
       </div>
     </section>
---
+++ src/components/HomeDark/Partners/partners.ts
@@ -0,0 +1,355 @@
+import bscPadLD from '../../../assets/img/icons/partners/bsc-pad.svg';
+import hyperconnectD from '../../../assets/img/icons/partners/hyperconnect-dark.svg';
+import hyperconnectL from '../../../assets/img/icons/partners/hyperconnect-light.svg';
+import lossLessD from '../../../assets/img/icons/partners/lossless-dark.svg';
+import lossLessL from '../../../assets/img/icons/partners/lossless-light.svg';
+import marshlandD from '../../../assets/img/icons/partners/marshland-dark.png';
+import marshlandL from '../../../assets/img/icons/partners/marshland-light.png';
+import moonwolfD from '../../../assets/img/icons/partners/moonwolf-dark.png';
+import moonwolfL from '../../../assets/img/icons/partners/moonwolf-light.png';
+import bscStationL from '../../../assets/img/icons/partners/bsc-station-light.png';
+import bscStationD from '../../../assets/img/icons/partners/bsc-station-dark.png';
+import polygonD from '../../../assets/img/icons/partners/polygon-dark.svg';
+import polygonL from '../../../assets/img/icons/partners/polygon-light.svg';
+import qiDaoD from '../../../assets/img/icons/partners/qi-dao-dark.png';
+import mateLD from '../../../assets/img/icons/partners/mate-dark.png';
+import orakuruLD from '../../../assets/img/icons/partners/orakuru-dark.png';
+import widilandLD from '../../../assets/img/icons/partners/widiland.png';
+import realmD from '../../../assets/img/icons/partners/realm-dark.png';
+import realmL from '../../../assets/img/icons/partners/realm-light.png';
+import qiDaoL from '../../../assets/img/icons/partners/qi-dao-light.png';
+import trustSwapD from '../../../assets/img/icons/partners/trust-swap-dark.svg';
+import unimexD from '../../../assets/img/icons/partners/unimex-light.svg';
+import unimexL from '../../../assets/img/icons/partners/unimex-dark.svg';
+import trustSwapL from '../../../assets/img/icons/partners/trust-swap-light.svg';
+import cyberfiL from '../../../assets/img/icons/partners/cyberfi-dark.svg';
+import cyberfiD from '../../../assets/img/icons/partners/cyberfi-light.svg';
+import apeswapLD from '../../../assets/img/icons/partners/apeswap-dark.svg';
+import certikD from '../../../assets/img/icons/partners/certik-dark.png';
+import certikL from '../../../assets/img/icons/partners/certik-light.png';
+import chainGamesD from '../../../assets/img/icons/partners/chaingames-dark.png';
+import chainGamesL from '../../../assets/img/icons/partners/chaingames-light.png';
+import oneringD from '../../../assets/img/icons/partners/onering-dark.png';
+import oneringL from '../../../assets/img/icons/partners/onering-light.png';
+import snowballD from '../../../assets/img/icons/partners/snowball-dark.png';
+import snowballL from '../../../assets/img/icons/partners/snowball-light.png';
+import penguinfinanceLD from '../../../assets/img/icons/partners/penguinfinance.png';
+import pangolinLD from '../../../assets/img/icons/partners/pangolin.png';
+import axialLD from '../../../assets/img/icons/partners/axial.png';
+import unipilotLD from '../../../assets/img/icons/partners/unipilot.png';
+import avalabsD from '../../../assets/img/icons/partners/avalabs-dark.png';
+import avalabsL from '../../../assets/img/icons/partners/avalabs-light.png';
+import baseProtocolLD from '../../../assets/img/icons/partners/baseprotocol.png';
+
+export const partnersSetArray = <const>[
+  'Binance Smart Chain',
+  'Multichain',
+  'Avalanche',
+  'Ethereum',
+  'Polygon',
+  'Advisors / Investors / Other',
+];
+export type TPartnerSet = typeof partnersSetArray[number];
+// export type TPartnerSet =
+//   | 'Binance Smart Chain'
+//   | 'Multichain'
+//   | 'Avalanche'
+//   | 'Ethereum'
+//   | 'Polygon'
+//   | 'Advisors / Investors / Other';
+
+interface IPartner {
+  name: string;
+  image: {
+    light: string;
+    dark: string;
+  };
+  link: string;
+  width: number;
+  height: number;
+}
+
+export const partners: Record<TPartnerSet, IPartner[]> = {
+  'Binance Smart Chain': [
+    {
+      name: 'bscpad',
+      image: {
+        light: bscPadLD,
+        dark: bscPadLD,
+      },
+      link: 'https://bscpad.com/',
+      width: 152,
+      height: 64,
+    },
+    {
+      name: 'orakuru',
+      image: {
+        light: orakuruLD,
+        dark: orakuruLD,
+      },
+      link: 'https://orakuru.io/',
+      width: 118,
+      height: 118,
+    },
+    {
+      name: 'ape swap',
+      image: {
+        light: apeswapLD,
+        dark: apeswapLD,
+      },
+      link: 'https://apeswap.finance/',
+      width: 315,
+      height: 84,
+    },
+    {
+      name: 'usemate',
+      image: {
+        light: mateLD,
+        dark: mateLD,
+      },
+      link: 'https://usemate.com/',
+      width: 182,
+      height: 51,
+    },
+    {
+      name: 'widiland',
+      image: {
+        light: widilandLD,
+        dark: widilandLD,
+      },
+      link: 'https://widiland.com/',
+      width: 274,
+      height: 96,
+    },
+    {
+      name: 'bscstation',
+      image: {
+        light: bscStationL,
+        dark: bscStationD,
+      },
+      link: 'https://bscstation.finance/#/Dashboard',
+      width: 186,
+      height: 72,
+    },
+  ],
+  'Multichain': [
+    {
+      name: 'realm',
+      image: {
+        light: realmL,
+        dark: realmD,
+      },
+      link: 'https://www.realm.art/',
+      width: 162,
+      height: 45,
+    },
+    {
+      name: 'chaingames',
+      image: {
+        light: chainGamesL,
+        dark: chainGamesD,
+      },
+      link: 'https://chaingames.io/',
+      width: 393,
+      height: 62,
+    },
+    {
+      name: 'onering',
+      image: {
+        light: oneringL,
+        dark: oneringD,
+      },
+      link: 'https://www.onering.finance/',
+      width: 287,
+      height: 66,
+    },
+    {
+      name: 'cyberfi',
+      image: {
+        light: cyberfiL,
+        dark: cyberfiD,
+      },
+      link: 'https://cyberfi.tech/',
+      width: 262,
+      height: 72,
+    },
+  ],
+  'Avalanche': [
+    {
+      name: 'snowball',
+      image: {
+        light: snowballL,
+        dark: snowballD,
+      },
+      link: 'https://www.snowball.network/',
+      width: 178,
+      height: 43,
+    },
+    {
+      name: 'penguinfinance',
+      image: {
+        light: penguinfinanceLD,
+        dark: penguinfinanceLD,
+      },
+      link: 'https://penguinfinance.org/',
+      width: 379,
+      height: 89,
+    },
+    {
+      name: 'pangolin',
+      image: {
+        light: pangolinLD,
+        dark: pangolinLD,
+      },
+      link: 'https://pangolin.exchange/',
+      width: 274,
+      height: 79,
+    },
+    {
+      name: 'axial',
+      image: {
+        light: axialLD,
+        dark: axialLD,
+      },
+      link: 'https://www.axial.exchange/',
+      width: 186,
+      height: 82,
+    },
+  ],
+  'Ethereum': [
+    {
+      name: 'lossless',
+      image: {
+        light: lossLessL,
+        dark: lossLessD,
+      },
+      link: 'https://lossless.cash/',
+      width: 245,
+      height: 54,
+    },
+    {
+      name: 'trustswap',
+      image: {
+        light: trustSwapL,
+        dark: trustSwapD,
+      },
+      link: 'https://trustswap.com/',
+      width: 276,
+      height: 46,
+    },
+    {
+      name: 'unipilot',
+      image: {
+        light: unipilotLD,
+        dark: unipilotLD,
+      },
+      link: 'https://unipilot.io/',
+      width: 336,
+      height: 48,
+    },
+    {
+      name: 'unimex',
+      image: {
+        light: unimexL,
+        dark: unimexD,
+      },
+      link: 'https://unimex.network/',
+      width: 239,
+      height: 64,
+    },
+  ],
+  'Polygon': [
+    {
+      name: 'moonwolf',
+      image: {
+        light: moonwolfL,
+        dark: moonwolfD,
+      },
+      link: 'https://moonwolf.io/',
+      width: 275,
+      height: 72,
+    },
+    {
+      name: 'qi dao',
+      image: {
+        light: qiDaoL,
+        dark: qiDaoD,
+      },
+      link: 'https://app.mai.finance/',
+      width: 160,
+      height: 72,
+    },
+    {
+      name: 'polygon',
+      image: {
+        light: polygonL,
+        dark: polygonD,
+      },
+      link: 'https://polygon.technology/',
+      width: 235,
+      height: 52,
+    },
+  ],
+  'Advisors / Investors / Other': [
+    {
+      name: 'avalabs',
+      image: {
+        light: avalabsL,
+        dark: avalabsD,
+      },
+      link: 'https://www.avalabs.org/',
+      width: 137,
+      height: 78,
+    },
+    {
+      name: 'marshland',
+      image: {
+        light: marshlandL,
+        dark: marshlandD,
+      },
+      link: 'https://www.marshlandcapital.com/',
+      width: 90,
+      height: 77,
+    },
+    {
+      name: 'baseprotocol',
+      image: {
+        light: baseProtocolLD,
+        dark: baseProtocolLD,
+      },
+      link: 'https://www.baseprotocol.org/',
+      width: 92,
+      height: 111,
+    },
+    {
+      name: 'certik',
+      image: {
+        light: certikL,
+        dark: certikD,
+      },
+      link: 'https://www.certik.com/',
+      width: 311,
+      height: 73,
+    },
+    {
+      name: 'icon hyperconnect',
+      image: {
+        light: hyperconnectL,
+        dark: hyperconnectD,
+      },
+      link: '', // TODO: add link
+      width: 290,
+      height: 64,
+    },
+    {
+      name: 'unimex',
+      image: {
+        light: unimexL,
+        dark: unimexD,
+      },
+      link: 'https://unimex.network/',
+      width: 239,
+      height: 64,
+    },
+  ],
+};
